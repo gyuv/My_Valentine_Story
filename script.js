38571abee19bb630a494b1701a760599d89499ec
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-02-11 14:44:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const observerOptions = {
-    threshold: 0.2
-};
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-        }
-    });
-}, observerOptions);
-
-document.querySelectorAll('.story-segment').forEach(section => {
-    observer.observe(section);
-});
