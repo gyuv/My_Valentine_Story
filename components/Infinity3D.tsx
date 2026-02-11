38a0aed934c0babab116c61559fb708ef4201508
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-02-11 15:31:17 +0530

    Update Infinity3D.tsx

--- components/Infinity3D.tsx
+++ components/Infinity3D.tsx
@@ -1,19 +1,17 @@
 "use client";
-
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
 export default function Infinity3D() {
   const ref = useRef<any>();
-
   useFrame(() => {
-    ref.current.rotation.y += 0.01;
+    if(ref.current) ref.current.rotation.y += 0.01;
   });
 
   return (
-    <mesh ref={ref} position={[0, -5, -10]}>
-      <torusKnotGeometry args={[3, 1, 200, 32]} />
-      <meshStandardMaterial color="#ff2e88" />
+    <mesh ref={ref} position={[0,-5,-10]}>
+      <torusKnotGeometry args={[3,1,200,32]} />
+      <meshStandardMaterial color="hotpink" />
     </mesh>
   );
 }
