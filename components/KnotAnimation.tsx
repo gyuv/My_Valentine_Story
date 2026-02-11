62e00f27e609484cc303c2c51644bd009921c014
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-02-11 15:43:49 +0530

    Update KnotAnimation.tsx

--- components/KnotAnimation.tsx
+++ components/KnotAnimation.tsx
@@ -1,15 +1,7 @@
-"use client";
-import { motion } from "framer-motion";
-
 export default function KnotAnimation() {
   return (
-    <motion.div
-      initial={{ scale: 0 }}
-      whileInView={{ scale: 1 }}
-      transition={{ duration: 2 }}
-      className="text-center text-6xl"
-    >
+    <div className="h-screen flex items-center justify-center text-6xl">
       🧵🤝💍
-    </motion.div>
+    </div>
   );
 }
