53e772785b4aed69a6ff7f57eaaa7a30fbbe1d14
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-02-11 14:24:52 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,15 @@
+const observerOptions = {
+    threshold: 0.2
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, observerOptions);
+
+document.querySelectorAll('.story-segment').forEach(section => {
+    observer.observe(section);
+});
