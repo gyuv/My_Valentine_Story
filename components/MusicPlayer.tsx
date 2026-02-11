ffac47a258715c79bc12dd6712e596cffd2c7ac4
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-02-11 15:13:33 +0530

    Create MusicPlayer.tsx

---
+++ components/MusicPlayer.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function MusicPlayer() {
+  useEffect(() => {
+    const audio = new Audio("/bg-music.mp3");
+    audio.loop = true;
+    audio.volume = 0.4;
+    audio.play().catch(()=>{});
+  }, []);
+
+  return null;
+}
