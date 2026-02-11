fa6c890c1f4eac04ccd0e1361993ea2ab41c717f
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-02-11 15:26:19 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
 import PasswordGate from "@/components/PasswordGate";
-import Starfield from "@/components/Starfield";
-import AutoNarration from "@/components/AutoNarration";
-import KnotAnimation from "@/components/KnotAnimation";
+import DolbyIntro from "@/components/DolbyIntro";
 import MusicPlayer from "@/components/MusicPlayer";
+import SoundFX from "@/components/SoundFX";
+import Starfield from "@/components/Starfield";
+import GoldNames from "@/components/GoldNames";
+import RainLightning from "@/components/RainLightning";
+import CinematicSections from "@/components/CinematicSections";
+import DroneTemple from "@/components/DroneTemple";
+import KnotAnimation from "@/components/KnotAnimation";
+import Ending from "@/components/Ending";
 
 export default function Page() {
   return (
     <PasswordGate>
+      <DolbyIntro />
       <MusicPlayer />
+      <SoundFX />
       <Starfield />
-      <AutoNarration />
+      <GoldNames />
+      <CinematicSections />
+      <RainLightning />
+      <DroneTemple />
       <KnotAnimation />
+      <Ending />
     </PasswordGate>
   );
 }
