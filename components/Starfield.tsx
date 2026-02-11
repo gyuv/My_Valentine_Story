a2b14d60252fa7c2bc82c4eaa84d8dbfbb4ed8ff
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-02-11 15:15:16 +0530

    Update Starfield.tsx

--- components/Starfield.tsx
+++ components/Starfield.tsx
@@ -1,22 +1,22 @@
-"use client";
-
-import { Canvas } from "@react-three/fiber";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import { Stars } from "@react-three/drei";
-import GalaxyWarp from "./GalaxyWarp";
-import MouseParticles from "./MouseParticles";
-import Infinity3D from "./Infinity3D";
-import TempleScene3D from "./TempleScene3D";
 
-export default function Starfield() {
-  return (
-    <div className="fixed top-0 left-0 w-full h-full -z-10">
-      <Canvas camera={{ position: [0, 0, 20], fov: 60 }}>
-        <Stars radius={200} depth={80} count={7000} factor={6} fade />
-        <GalaxyWarp />
-        <MouseParticles />
-        <Infinity3D />
-        <TempleScene3D />
-      </Canvas>
-    </div>
-  );
-}
+<Canvas camera={{ position: [0, 0, 20], fov: 60 }}>
+  <ambientLight intensity={0.5} />
+  <pointLight position={[10, 10, 10]} intensity={2} />
+
+  <Stars radius={200} depth={80} count={7000} factor={6} fade />
+
+  <Infinity3D />
+  <TempleScene3D />
+  <GalaxyWarp />
+  <MouseParticles />
+
+  <EffectComposer>
+    <Bloom
+      intensity={1.8}
+      luminanceThreshold={0.2}
+      luminanceSmoothing={0.9}
+    />
+  </EffectComposer>
+</Canvas>
