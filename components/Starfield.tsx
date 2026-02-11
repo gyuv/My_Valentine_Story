3bce2aa5fe3ade05ef99c4998ac19d3893797105
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-02-11 15:09:56 +0530

    Update Starfield.tsx

--- components/Starfield.tsx
+++ components/Starfield.tsx
@@ -1,43 +1,21 @@
 "use client";
 
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { Stars } from "@react-three/drei";
-import { useRef } from "react";
-import * as THREE from "three";
-
-function FloatingParticles() {
-  const ref = useRef<any>();
-
-  useFrame(() => {
-    if (ref.current) {
-      ref.current.rotation.y += 0.0005;
-      ref.current.rotation.x += 0.0003;
-    }
-  });
-
-  return (
-    <points ref={ref}>
-      <bufferGeometry>
-        <bufferAttribute
-          attach="attributes-position"
-          count={500}
-          array={new Float32Array(
-            Array.from({ length: 1500 }, () => (Math.random() - 0.5) * 200)
-          )}
-          itemSize={3}
-        />
-      </bufferGeometry>
-      <pointsMaterial color="#ff2e88" size={0.5} />
-    </points>
-  );
-}
+import GalaxyWarp from "./GalaxyWarp";
+import MouseParticles from "./MouseParticles";
+import Infinity3D from "./Infinity3D";
+import TempleScene3D from "./TempleScene3D";
 
 export default function Starfield() {
   return (
     <div className="fixed top-0 left-0 w-full h-full -z-10">
-      <Canvas camera={{ position: [0, 0, 20] }}>
-        <Stars radius={100} depth={50} count={5000} factor={4} fade />
-        <FloatingParticles />
+      <Canvas camera={{ position: [0, 0, 20], fov: 60 }}>
+        <Stars radius={200} depth={80} count={7000} factor={6} fade />
+        <GalaxyWarp />
+        <MouseParticles />
+        <Infinity3D />
+        <TempleScene3D />
       </Canvas>
     </div>
   );
